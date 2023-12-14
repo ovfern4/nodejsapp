b6d42aa8bd1861ce698e86a48acc5f1e09b187be
ovfern4 <152268646+ovfern4@users.noreply.github.com>
2023-12-14 10:35:36 +0100

    Update nodejsDockerDSL.groovy con mi nombre de usuario

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/ovfern4/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('ovfern4')
+            node / gitConfigEmail('ovfern4@mapfre.com')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('macloujulian/nodejsapp')
+            repositoryName('ovfern4/nodejsapp')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
