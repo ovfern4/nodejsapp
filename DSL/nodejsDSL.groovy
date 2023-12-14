40a716652cd3d9d6793e37f2c2b1c6f60df11da9
ovfern4 <152268646+ovfern4@users.noreply.github.com>
2023-12-14 10:34:40 +0100

    Update nodejsDSL.groovy con mi nombre de usuario

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/ovfern4/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('ovfern4')
+            node / gitConfigEmail('ovfern4@mapfre.com')
         }
     }
     triggers {
